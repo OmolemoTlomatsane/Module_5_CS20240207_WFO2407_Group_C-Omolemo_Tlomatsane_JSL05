3721cd997d07f9890923abe85a332a5816d9d334
Omolemo Tlomatsane <tycotecsa@gmail.com>
2024-09-17 10:13:02 +0200

    Created song selection.

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ const songs = [
     { title: "I'm Not in Love", artist: "10cc", genre: "Pop" },
     { title: "Fooled Around and Fell in Love", artist: "Elvin Bishop", genre: "Rock" },
     // Feel free to add even more songs
+    {title: "Hotel Carlifonia", artist: "Eagles", genre: "Rock"},
+    {title: "Superstition", artist: "Stevie Wonder", genre: "Soul"},
+    { title: "Dancing Queen", artist: "ABBA", genre: "Pop" },
+    { title: "Bohemian Rhapsody", artist: "Queen", genre: "Rock" },
+    { title: "The Chain", artist: "Fleetwood Mac", genre: "Rock" }
 ];
 
 
@@ -20,12 +25,44 @@ const guardians = {
     "Star-Lord": "Rock",
     "Gamora": "Pop",
     // Add preferences for Drax, Rocket, and Groot
+    "Drax": "Soul",
+    "Rocket": "Rock",
+    "Groot": "Pop"
 };
 
 // Function to generate playlist based on preferred genre
 function generatePlaylist(guardians, songs) {
     // Use the map() function to create playlists for each Guardian
     // Your code here
+    const playlistsDiv = document.getElementById("playlists")
+    playlistsDiv.innerHTML = '' //Clear some existing content
+
+    Object.keys(guardians).map(guardian => {
+        const genrePreference = guardians[guardian]
+        const guardianPlaylist = songs.filter(song => song.genre === genrePreference)
+     //Generate playlist div
+     const playlistDiv = document.createElement('div')
+     playlistDiv.classList.add('playlist')
+
+     //Add guardian's name as the playlist title
+     const playlistTitle = document.createElement('h3')
+     playlistTitle.innerHTML = `${guardian}'s playlist`
+     playlistDiv.appendChild(playlistTitle)
+    if (guardianPlaylist.length > 0){
+         //Add songs to the playlist
+        guardianPlaylist.forEach(song => {
+        const songElement = document.createElement('p')
+        songElement.innerHTML = `<a href="#">${song.title}</a> By ${song.artist}`
+        playlistDiv.appendChild(songElement)});
+    }
+    else{
+        // Show a message if no songs are available
+        const noSongsMessage = document.createElement("p");
+        noSongsMessage.innerText = "No songs available for this genre.";
+        playlistDiv.appendChild(noSongsMessage);
+    }
+     playlistsDiv.appendChild(playlistDiv)
+    })
 }
 
 // Call generatePlaylist and display the playlists for each Guardian
